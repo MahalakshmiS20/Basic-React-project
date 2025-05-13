536ad417ea1c8fd255f4d44f840fcfb41a818db5
MahalakshmiS20 <mahalakshmisenthil208@gmail.com>
2025-05-13 12:50:16 +0530

    image path changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Header= ()=>{
     return(
-        <header className="h-96 bg-cover bg-center flex items-center justify-center" style={{backgroundImage:"url('/images.jpg')"}}>
+        <header className="h-96 bg-cover bg-center flex items-center justify-center" style={{backgroundImage:"url('./images.jpg')"}}>
             <h1 className="text-3xl font-bold text-white border rounded-xl bg-black/50 backdrop:blur-lg p-4">🚀Welcome to my Website</h1>
         </header>
     )
